9f4a0aeaa3967ff3f231550c620495d71b631549
Aisha Saleem <aisaleem@macrosoftonic.com>
2023-07-13 12:12:05 +0500

    resume link updated

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -102,7 +102,7 @@ const Experience = () => {
               ease-in-out"
               onClick={() =>
                 window.open(
-                  'resume link', //paste the link to your resume here
+                  'https://drive.google.com/file/d/17GVEsamFrxumCw1YOM38HJBIkbJyuoAj/view?usp=drivesdk', //paste the link to your resume here
                   '_blank'
                 )
               }
